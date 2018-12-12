44bee3c00a7d4d37d1cdc8ec01e19066fa8329c0
dapeng <fengzq@vdaoyun.com>
2018-12-12 14:10:15 +0800

    1. 优化从错误页到登录页中登录之后，无法跳转至最初页面的BUG

--- src/views/error.vue
+++ src/views/error.vue
@@ -1,13 +1,26 @@
 <template>
     <div class="error">
       <svg-icon icon-class="error"/>
-      <p>微信授权失败，试试手动登录吧~ <router-link to="login">登录</router-link></p>
+      <p>微信授权失败，试试手动登录吧~ <router-link :to="{path: 'login', query: {redirect: redirect}}">登录</router-link></p>
     </div>
 </template>
 
 <script>
   export default {
-    name: 'error'
+    name: 'error',
+    data () {
+      return {
+        redirect: '/'
+      }
+    },
+    watch: {
+      $route: {
+        handler: function (route) {
+          this.redirect = route.query && route.query.redirect
+        },
+        immediate: true
+      }
+    }
   }
 </script>
 
